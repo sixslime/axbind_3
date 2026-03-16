e685c29abc4dbb5a4e21cbbe246c23bc22a8e7c8
SixSlime <ryan.mm.taylor@gmail.com>
2026-03-16 15:30:30 -0700

    -

--- Types/ConfigFile.cs
+++ Types/ConfigFile.cs
@@ -1,6 +1,18 @@
-namespace SixSlime.AxBind3.Types;
+﻿namespace SixSlime.AxBind3.Types;
 using Tomlyn.Serialization;
+using Tomlyn;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 public class ConfigFile
 {
     public List<Pass> Passes { get; set; } = [];
+
+    public static ConfigFile FromToml(string toml)
+    {
+        return TomlSerializer.Deserialize(toml, AppTomlContext.Default.ConfigFile)!;
+    }
 }
+[TomlSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower)]
+[TomlSerializable(typeof(ConfigFile))]
+internal partial class AppTomlContext : TomlSerializerContext;
