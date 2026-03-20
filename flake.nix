fdf2a794c81364d3ccc56932f8dd2d6b3c2e9082
SixSlime <ryan.mm.taylor@gmail.com>
2026-03-19 20:59:38 -0700

    i am NOT doing all that just for native aot. ill take my fucking 50mb binary and be happy. kys and also kill yourself.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+    description = "axbind3";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = nixpkgs.legacyPackages.${system};
+        in {
+            packages.default = pkgs.buildDotnetModule {
+                pname = "axbind3";
+                version = "0.1.0";
+                src = ./.;
+
+                projectFile = "./SixSlime.AxBind3.csproj";
+                nugetDeps = ./deps.json;
+
+                dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0;
+
+                selfContainedBuild = true;
+            };
+
+            devShells.default = pkgs.mkShell {
+                packages = [ pkgs.dotnetCorePackages.sdk_8_0 ];
+            };
+        });
+}
